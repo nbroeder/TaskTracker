be29185beba48fe107faef03b4e0f817bde7d14c
nbroeder <nbroeder@uwo.ca>
2021-07-27 21:30:08 -0400

    added a task list component that is filled with task components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import { useState } from 'react'
 
-const App = () =>{
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    }
+  ])
+
+
+  //Delete items
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((task)=>task.id !==id))
+  }
+
   return (
     <div className="App">
-        <Header/>
-        
-     </div>
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
+    </div>
   )
 }
 
