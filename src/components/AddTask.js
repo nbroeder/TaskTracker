a773a73892c93b48d4378c02569286e4d058edbe
nbroeder <nbroeder@uwo.ca>
2021-08-03 20:59:20 -0400

    can now add new tasks as well as toggle the form to create tasks on and off

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,21 +1,44 @@
+import {useState} from 'react'
 
+const AddTask = ({onAdd }) => {
 
-const AddTask = () => {
+    const [text,setText]=useState('')
+    const [day,setDay]=useState('')
+    const [reminder,setReminder]=useState(false)
+    
+
+    const onSubmit=(e)=>{
+        e.preventDefault()
+
+        if(!text){
+            alert('please add a task')
+            return
+        }
+
+        onAdd({text,day,reminder})
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
     return (
-        <form classNme='add-Form'>
+        <form className='add-Form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task'/>
+                <input type='text' placeholder='Add Task' 
+                    value={text} onChange={(e)=>setText(e.target.value)} />
             </div>
 
             <div className='form-control'>
                 <label>Date & Time</label>
-                <input type='text' placeholder='Add Date and Time'/>
+                <input type='text' placeholder='Add Date and Time' 
+                    value={day} onChange={(e)=>setDay(e.target.value)}/>
+                    
             </div>
 
             <div className='form-control form-control-check     '>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' checked={reminder} value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)}/>
             </div>
 
             <input className='btn btn-block'type='submit' value='Save Task'/>
